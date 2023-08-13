d4e7b7e79c9b16e9c4586fcc5a009cd66296fcf4
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2023-08-13 11:34:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Windows AMI ID"
+   default     = "ami-09e14f3154e091177"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
